09109e95f957fa2387ad216333ad0ad26cbbb79f
V. <wmswiderska@gmail.com>
2013-06-13 00:00:49 +0200

    empty strategy 2

---
+++ src/strategies/strategies/strategies/TakeCheapestThenOfferLowestOffer.java
@@ -0,0 +1,37 @@
+package strategies.strategies;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import agents.AgentOffer;
+
+public class TakeCheapestThenOfferLowestOffer extends Strategy {
+
+	@Override
+	public ArrayList<AgentOffer> decideAboutSellOffer(
+			ArrayList<AgentOffer> offers) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Map<String, Boolean> composeFinalBuyingDecision(
+			ArrayList<AgentOffer> sellOffers) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void updateBuyerStore(String traderName) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public ArrayList<AgentOffer> decideAboutBuyOffer(
+			ArrayList<AgentOffer> offers) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
