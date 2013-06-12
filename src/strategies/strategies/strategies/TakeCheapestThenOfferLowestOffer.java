c85fca1e15c015308aa061da4f14bba5f596b3d8
V. <wmswiderska@gmail.com>
2013-06-13 00:04:02 +0200

    and seller 2

--- src/strategies/strategies/strategies/TakeCheapestThenOfferLowestOffer.java
+++ src/strategies/strategies/strategies/TakeCheapestThenOfferLowestOffer.java
@@ -1,9 +1,11 @@
 package strategies.strategies;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Map;
 
 import agents.AgentOffer;
+import constants.Products;
 
 public class TakeCheapestThenOfferLowestOffer extends Strategy {
 
@@ -30,8 +32,26 @@ public class TakeCheapestThenOfferLowestOffer extends Strategy {
 	@Override
 	public ArrayList<AgentOffer> decideAboutBuyOffer(
 			ArrayList<AgentOffer> offers) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<AgentOffer> answer = new ArrayList<AgentOffer>();
+		Iterator<Products> sellIterator = sell.keySet().iterator();
+		while (sellIterator.hasNext()) {
+			Products product = sellIterator.next();
+			Iterator<AgentOffer> offersIterator = offers.iterator();
+			while (offersIterator.hasNext()) {
+				AgentOffer offer = offersIterator.next();
+				AgentOffer currentAnswer = new AgentOffer(offer.getAgentName(),
+						"");
+				if (offer.getItemPrice().get(product) >= 0.9 * pricePerItem
+						.get(product)) {
+					currentAnswer.addItemAmount(product, sell.get(product));
+					currentAnswer.addItemPrice(product, offer.getItemPrice()
+							.get(product));
+				}
+				answer.add(currentAnswer);
+			}
+
+		}
+		return answer;
 	}
 
 }
